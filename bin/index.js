17ea3d7bd640d18938c1408b00313d01d66cc27b
nocodeempire <tioy102@gmail.com>
2022-09-02 13:27:08 +0800

    feat: upgrade

--- bin/index.js
+++ bin/index.js
@@ -2,12 +2,14 @@
 
 const { program } = require('commander');
 const inquirer = require('inquirer');
+const chalk = require('chalk');
 const cherryPick = require('../lib/cherry-pick');
 const downloadTemp = require('../lib/download-temp');
+const upgradeTemp = require('../lib/upgrade-temp');
 
 program
   .name('amechlcli')
-  .description('CLI to some JavaScript string utilities')
+  .description('CLI to some JavaScript utilities')
   .version('1.0.0')
   .showHelpAfterError();
 
@@ -23,6 +25,23 @@ program.command('init <project>')
       message: '初始化模板: '
     }])
     downloadTemp(projectName, res.temp);
+  })
+
+program.command('upgrade')
+  .option('-f, --force', '强制更新')
+  .description('更新rnext')
+  .action(async (arg, ...args) => {
+    if(arg && arg.force) {
+      upgradeTemp()
+    } else {
+      const res = await inquirer.prompt([{
+        type: 'confirm',
+        name: 'value',
+        message: '更新将覆盖本地rnext文件, 确认更新么？'
+      }])
+      if(res.value) upgradeTemp();
+      else console.log(chalk.cyanBright('暂不更新'));
+    }
   })
 
 program.command('cherry-pick <branch>')
